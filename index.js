2cde7d494c619e657954779f1f468a3a49d2d3ff
abigail <abigail.wamai@student.moringaschool.com>
2023-06-15 08:28:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+function upperCaseCustomerName (){
+    return customerName.toUpperCase;
+}
+function setBestCustomer(){
+    return window.bestCustomer = `not bob`
+}
+function overwriteBestCustomer(){
+    return window.bestCustomer = `maybe bob`
+}
+const leastFavoriteCustomer = `her`
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer = `him`
+}
